2647bb23f168e03029c84d8ef1cfe800f92353e9
Michelle <michelle.adeline.25@gmail.com>
2020-05-15 13:01:53 +0700

    fixed grammatical errors

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -90,8 +90,8 @@ export const RECIPES = [
         ],
         [
           'Form 2 patties',
-          'Fry the patties for c. 4 minutes on each side',
-          'Quickly fry the buns for c. 1 minute on each side',
+          'Fry the patties for about 4 minutes on each side',
+          'Quickly fry the buns for about 1 minute on each side',
           'Bruch buns with ketchup',
           'Serve burger with tomato, cucumber and onion'
         ],
